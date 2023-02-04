cb447ffdc23d25ba93fabbb9e3c77b1fe910538e
Miniieee <bagyi.tomi93@gmail.com>
2023-02-04 14:06:02 +0000

    Collision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,8 +13,40 @@ public class Player : MonoBehaviour
         Vector2 inputvector = gameInput.GetMovementVectorNormalized();
         
         Vector3 moveDir = new Vector3(inputvector.x, 0f, inputvector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up* playerHeight, playerRadius, moveDir, moveDistance);
+       
+        if (!canMove)
+        {   //Attempt X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up* playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {   //can move only in X
+                moveDir = moveDirX;
+            } else
+                {   //Attempt Z movement
+                    Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                    canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up* playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                    if (canMove)
+                    {   //can move only in z dir
+                        moveDir = moveDirZ;
+                    } else
+                        {
+                            //can not move
+                        }
+
+                }
+        }
+        if (canMove)
+        {
+           transform.position += moveDir * moveSpeed * Time.deltaTime; 
+        }
+        
         isWAlking = moveDir != Vector3.zero;
         transform.forward = Vector3.Lerp(transform.forward, moveDir, turnSpeed * Time.deltaTime);
     }
