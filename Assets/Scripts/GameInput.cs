d9f42ea0d086eb2464c184d99c8566e8d199fe79
Miniieee <bagyi.tomi93@gmail.com>
2023-02-04 13:32:26 +0000

    Implemented Input System

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -4,30 +4,17 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private PlayerInputActions playerInputActions;
+
+    private void Awake() {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
         Vector2 inputvector = new Vector2(0,0);
-
-
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputvector.y = +1;
-        }
-
-        if(Input.GetKey(KeyCode.S))
-        {
-            inputvector.y = -1;
-        }
-
-        if(Input.GetKey(KeyCode.A))
-        {
-            inputvector.x = -1;
-        }
-
-        if(Input.GetKey(KeyCode.D))
-        {
-            inputvector.x = +1;
-        } 
+        inputvector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
         inputvector = inputvector.normalized;
 
